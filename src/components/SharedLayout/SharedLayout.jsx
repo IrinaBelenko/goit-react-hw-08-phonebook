354e7dc44f31966682c57df913cfee2e13f4222e
IrinaBelenko <118209690+IrinaBelenko@users.noreply.github.com>
2023-08-21 13:42:18 +0300

    styles

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,21 +1,27 @@
-import { NavLink, Outlet } from 'react-router-dom';
-
+import { Outlet } from 'react-router-dom';
 import { Suspense } from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import { NavLinkStyled } from './SharedLayout.styled';
 
 export const SharedLayout = () => {
   return (
     <>
-      <header>
-        <div>
-          <div>
-            <NavLink to="/" end>
-              Contacts
-            </NavLink>
-            <NavLink to="/login">Login</NavLink>
-            <NavLink to="/register">Register</NavLink>
-          </div>
-        </div>
-      </header>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              <NavLinkStyled to="/" end>
+                Contacts
+              </NavLinkStyled>
+            </Typography>
+            <NavLinkStyled to="/login">Login</NavLinkStyled>
+            <NavLinkStyled to="/register">Register</NavLinkStyled>
+          </Toolbar>
+        </AppBar>
+      </Box>
       <Suspense>
         <Outlet />
       </Suspense>
--- src/components/SharedLayout/SharedLayout.styled.js
+++ src/components/SharedLayout/SharedLayout.styled.js
@@ -41,26 +41,27 @@ export const HeaderWrapper = styled.div`
 `;
 
 export const NavLinkStyled = styled(NavLink)`
-  color: ${({ theme }) => theme.colors.light};
-  font-size: ${({ theme }) => theme.fontSizes.medium};
+  color: #fff;
+  font-size: 18px;
   font-weight: 700;
 
+  text-decoration: none;
   text-transform: uppercase;
 
   transition-property: color, background-color, transform;
   transition-duration: 0.2s;
-  transition-timing-function: ${({ theme }) => theme.animations.cubicBezier};
+  transition-timing-function: '0.25s cubic-bezier(0.7, 0.98, 0.86, 0.98)';
 
   transform: scale(0.9);
 
   &:hover,
   &:focus {
-    color: ${({ theme }) => theme.colors.white};
+    color: #f2f2f2;
   }
 
   &.active {
-    color: ${({ theme }) => theme.colors.white};
-    background-color: ${({ theme }) => theme.colors.accent};
+    color: #f2f2f2;
+    background-color: #000080;
 
     padding: 6px 12px;
     border-radius: 4px;
